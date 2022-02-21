65b62e543592b4c84f3ec96257a2eaf9781b802a
FusionSid <siddheshadsv@icloud.com>
2022-02-21 19:33:08 +1300

    Trash endpoint working

--- routers/trash.py
+++ routers/trash.py
@@ -1,8 +1,40 @@
 from fastapi import APIRouter
-from fastapi.responses import FileResponse, Response
+from io import BytesIO
+from fastapi.responses import StreamingResponse
+from utils import get_url_image
+from PIL import Image
+import os
 
-trash = APIRouter()
+tags_metadata = [
+    {
+        "name": "Trash Meme",
+    }
+]
 
-@trash.get("/api/trash/", responses = {200: {"content": {"image/png": {}}}}, response_class=Response)
-def gen_trash_img():
-    return FileResponse("file_path", media_type="image/png")
+trash = APIRouter(tags=tags_metadata)
+
+async def generate_image(image_url : str):
+    profile_image = await get_url_image(image_url)
+    profile = BytesIO(profile_image)
+    profile.seek(0)
+    size = (480, 485)
+    avatar = Image.open(profile).resize(size)
+
+
+    cwd = os.getcwd()
+    img = Image.open(f"{cwd}/assets/trash.bmp")
+    img.paste(avatar, (480, 0))
+
+    d = BytesIO()
+    d.seek(0)
+    img.save(d, "PNG")
+    d.seek(0)
+    return d
+
+
+@trash.get("/api/trash/", responses = {200: {"content": {"image/png": {}}}}, response_class=StreamingResponse)
+async def gen_trash_img(image_url : str):
+    
+    file = await generate_image(image_url)
+
+    return StreamingResponse(file, media_type="image/png")

53809b75ae8056a4a856c5d9f3d5345523bb7d57
FusionSid <siddheshadsv@icloud.com>
2022-02-28 14:55:34 +1300

    Surprised meme working

--- routers/changemymind.py
+++ routers/changemymind.py
@@ -21,16 +21,33 @@ async def generate_image(text):
     cwd = os.getcwd()
 
     img = Image.open(f"{cwd}/assets/changemymind.bmp")
-    draw = ImageDraw.Draw(img)
     font = ImageFont.truetype(f"{cwd}/fonts/roboto-medium.ttf", 20)
 
-    w, h =670, 330
+    text_image = Image.new("RGB", (768, 568),color="WHITE")
+    text_image.convert("RGBA")
+
+    draw = ImageDraw.Draw(text_image)
+
+    newImage = []
+    for item in text_image.getdata():
+        if item[:3] == (255, 255, 255):
+            newImage.append((255, 255, 255, 0))
+        else:
+            newImage.append(item)
+
+    text_image.putdata(newImage)
+
+    w, h = 670, 330
     lines = textwrap.wrap(text, width=18)
     y_text = h
     for line in lines:
         width, height = font.getsize(line)
         draw.text(((w - width) / 2, y_text), line, font=font, fill="black", align="center")
         y_text += height
+    
+    text_image = text_image.rotate(20)
+  
+    text_image = Image.alpha_composite(img, text_image)
 
     d = BytesIO()
     d.seek(0)

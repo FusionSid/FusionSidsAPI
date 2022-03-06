332c554022cae723f769a5969d5475f734df1361
FusionSid <siddheshadsv@icloud.com>
2022-03-06 16:05:03 +1300

    Added system stats to /stats

--- routers/stats.py
+++ routers/stats.py
@@ -1,4 +1,6 @@
+import platform
 from fastapi import APIRouter
+import psutil
 from utils import get_db
 
 # Title for docs
@@ -13,6 +15,13 @@ stats = APIRouter(tags=tags_metadata)
 @stats.get("/stats/")
 async def get_stats():
     data = await get_db()
+    system_data = {
+    "memory" : f'{psutil.virtual_memory().percent}%',
+    "running_on" : f"{platform.system()} {platform.release()}",
+    "python_version" : f"{platform.python_version()}",
+    "cpu" : f'{psutil.cpu_percent()}%'
+    }
     return {
-        "stats" : data
+        "stats" : data,
+        "system_stats" : system_data
     }
